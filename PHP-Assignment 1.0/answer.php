ee385d9a6958d8f5e633e045e1f432f6e424c3e1
mosespace <kisakyemoses50@gmail.com>
2023-03-16 16:37:12 +0300

    completed_question_five

--- PHP-Assignment 1.0/answer.php
+++ PHP-Assignment 1.0/answer.php
@@ -153,4 +153,38 @@ echo '<br>' ."The total weight of apples sold in the week is " . $total_apples .
 
 echo '<br>' ."The total weight of apples sold in the week is ${total_apples} pounds.";
 
+
+/*---------- Question Five--------------------------------------------
+A gym wants to calculate the average weight lifted by members during a recent weightlifting competition. If there were 10 participants and their individual weights lifted were 200lbs, 225lbs, 180lbs, 190lbs, 215lbs, 210lbs, 195lbbs, 185lbs, 230lbs, and 240lbs, how can PHP variable be used to store this information, and how can concatenation be used to display the average weight lifted by participants?
+
+*/ 
+
+
+echo '<h1> Question Five</h1>';
+
+// To solve this question, i first defined an array variable to store the weights lifted by each participant.
+
+$weights = array(200, 225, 180, 190, 215, 210, 195, 185, 230, 240);
+
+
+echo '<br>' . $weights;
+// This step am initializing the variables to store the total weight and number of participants.
+$total_weight = 0;
+$num_participants = 0;
+
+// On this step, using my JS Knowledge, i Loop through the array of weights and add each weight to the total weight
+foreach ($weights as $weight) {
+    $total_weight += $weight;
+    $num_participants++;
+}
+
+// On finalising, i calculate the average weight by dividing the total weight by the number of participants.
+$average_weight = $total_weight / $num_participants;
+
+// Finally i display the average weight lifted by participants.
+echo "The average weight lifted by participants is " . $average_weight . " lbs.";
+
+// Or also the following method of concatenation method can be used to get the same results;
+
+echo '<br>' ."The average weight lifted by participants is ${average_weight} lbs.";
 ?>
