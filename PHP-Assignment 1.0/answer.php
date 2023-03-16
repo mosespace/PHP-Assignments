c54544d31c298f7c55c36c0b5fb9e91bf03cfa65
mosespace <kisakyemoses50@gmail.com>
2023-03-16 12:02:29 +0300

    added_question_one

---
+++ PHP-Assignment 1.0/answer.php
@@ -0,0 +1,8 @@
+<?php
+
+/*----------- Question One--------------------------------------------
+A local coffe shop wants to keep track of the total revenue earned from coffe sales in a day. If the shop sold 120 cups of coffee at $2.50 each, how can PHP variables be used to store this information and how can concatenation be used to display the total revenue earned?
+
+*/
+echo 'Hello World';
+?>
