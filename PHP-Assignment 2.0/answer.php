1087fc068d56253341a8d99fee1328758e6eafd6
mosespace <kisakyemoses50@gmail.com>
2023-03-17 12:41:07 +0300

    added question_one

---
+++ PHP-Assignment 2.0/answer.php
@@ -0,0 +1,22 @@
+<?php
+
+echo '<h1>Question One</h1>'
+/*---------- Question One--------------------------------------------
+Suppose a student has taken end-of-term exams in Math, English, Science, and Social
+Studies, and their scores are stored in four variables as follows:
+phpCopy code
+$mathScore = 80; // out of 100 $englishScore = 70; // out of 100 $scienceScore = 90;
+// out of 100 $socialStudiesScore = 75; // out of 100
+Write a PHP if statement to calculate the student's final grade based on the following
+grading scale:
+• A: average score >= 90
+• B: 80 <= average score < 90
+• C: 70 <= average score < 80
+• D: 60 <= average score < 70
+• F: average score < 60
+Your if statement should output the student's average score and final grade.
+
+
+*/
+
+?>
