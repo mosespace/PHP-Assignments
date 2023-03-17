180b1877c19bc57ea6e6520af985443f7203c68c
mosespace <kisakyemoses50@gmail.com>
2023-03-17 17:09:45 +0300

    added question one

---
+++ Lessons/Lesson five/index.php
@@ -0,0 +1,16 @@
+<?php
+// Functions
+// 1) Defination of a function
+// Example One ----------------------------------
+
+function calcAge(){
+    // Logic
+    $age = 2023-1950;
+
+    echo $age. '<br>';
+}
+// 2 Invoke the function
+calcAge();
+
+
+?>
